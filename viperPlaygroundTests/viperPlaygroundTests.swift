976ddfaba16d2f4a3e594b9737a0403f10ab844e
Bogdan Nikolaev <bogdannikolaev@macbook-pro-bogdan.local>
2018-10-24 14:31:26 +0300

    Initial Commit

---
+++ viperPlaygroundTests/viperPlaygroundTests.swift
@@ -0,0 +1,36 @@
+//
+//  viperPlaygroundTests.swift
+//  viperPlaygroundTests
+//
+//  Created by Bogdan Nikolaev on 24/10/2018.
+//  Copyright © 2018 Bogdan Nikolaev. All rights reserved.
+//
+
+import XCTest
+@testable import viperPlayground
+
+class viperPlaygroundTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
